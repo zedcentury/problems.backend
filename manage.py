01d7cbac69f98061826a16cf300360d954a45481
asliddin750750 <asliddin750750@gmail.com>
2023-08-24 16:13:50 +0500

    manage.py

--- manage.py
+++ manage.py
@@ -15,7 +15,11 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+
+    if sys.argv[1] == "runserver":
+        execute_from_command_line([sys.argv[0], "runserver", os.environ.get('URL', '127.0.0.1:8000')])
+    else:
+        execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
